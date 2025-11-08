88f31fae3fbeb1dff38a814179c44f3ff21e46b9
BillyHarrington1 <datskiydenis@gmail.com>
2025-11-09 02:29:39 +0500

    Task0

---
+++ Tyuiu.DatskiyDE.Sprint6.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.DatskiyDE.Sprint6.Task0.V22.Lib
+{
+    public class DataService : ISprint6Task0V22
+    {
+        public double Calculate(int x)
+        {
+            double dx = x;
+            double y = 0.7 * Math.Pow(dx, 3) + 1.52 * Math.Pow(dx, 2);
+            y = Math.Round(y, 3);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.DatskiyDE.Sprint6.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DatskiyDE.Sprint6.Task0.V22.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint6.Task0.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            double result = ds.Calculate(x);
+            double expected = 11.680;
+
+            Assert.AreEqual(expected, result, 0.001);
+        }
+    }
+}
