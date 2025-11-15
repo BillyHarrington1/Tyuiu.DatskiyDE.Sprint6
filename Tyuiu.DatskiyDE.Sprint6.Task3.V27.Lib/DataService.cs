932fbe261eafb26e9b0cb19becfed22ad6594bb3
BillyHarrington1 <datskiydenis@gmail.com>
2025-11-15 09:29:05 +0500

    Task3

--- Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
+++ Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
@@ -8,34 +8,16 @@ namespace Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
+            int[,] result = new int[rows, cols];
+
+            // Извлекаем 4-й столбец (индекс 3) и сортируем его
             int[] column3 = new int[rows];
             for (int i = 0; i < rows; i++)
             {
                 column3[i] = matrix[i, 3];
             }
 
-            Array.Sort(column3); 
-      
-            var valuesAndIndices = new (int value, int colIndex)[cols];
-
-            for (int j = 0; j < cols; j++)
-            {
-                valuesAndIndices[j] = (matrix[3, j], j);
-            }
-        
-            Array.Sort(valuesAndIndices, (a, b) => a.value.CompareTo(b.value));
-       
-            int[,] result = new int[rows, cols];
-
-            for (int j = 0; j < cols; j++)
-            {
-                int sourceColIndex = valuesAndIndices[j].colIndex;
-                for (int i = 0; i < rows; i++)
-                {
-                    result[i, j] = matrix[i, sourceColIndex];
-                }
-            }
-
+            Array.Sort(column3); // теперь column3 отсортирован         
             return result;
         }
     }
