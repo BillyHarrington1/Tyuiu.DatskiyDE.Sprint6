49c18bc8abbd7660b47ae84e7a25832b7bbcbb90
BillyHarrington1 <datskiydenis@gmail.com>
2025-11-15 09:26:05 +0500

    Task3

--- Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
+++ Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
@@ -8,18 +8,31 @@ namespace Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-            int[,] result = new int[rows, cols];
-
+            int[] column3 = new int[rows];
             for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < cols; j++)
+                column3[i] = matrix[i, 3];
+            }
+
+            Array.Sort(column3); 
+      
+            var valuesAndIndices = new (int value, int colIndex)[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                valuesAndIndices[j] = (matrix[3, j], j);
+            }
+        
+            Array.Sort(valuesAndIndices, (a, b) => a.value.CompareTo(b.value));
+       
+            int[,] result = new int[rows, cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                int sourceColIndex = valuesAndIndices[j].colIndex;
+                for (int i = 0; i < rows; i++)
                 {
-                    if (j == 3)
-                        result[i, j] = matrix[i, 4]; // 3-й столбец = 4-й столбец
-                    else if (j == 4)
-                        result[i, j] = matrix[i, 3]; // 4-й столбец = 3-й столбец
-                    else
-                        result[i, j] = matrix[i, j]; // остальные столбцы без изменений
+                    result[i, j] = matrix[i, sourceColIndex];
                 }
             }
 
