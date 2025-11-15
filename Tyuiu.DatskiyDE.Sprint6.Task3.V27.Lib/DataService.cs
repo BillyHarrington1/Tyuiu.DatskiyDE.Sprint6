1db91df172b6b5218662ec34111d4b1d114e082e
BillyHarrington1 <datskiydenis@gmail.com>
2025-11-15 09:33:47 +0500

    Task3

--- Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
+++ Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
@@ -8,30 +8,42 @@ namespace Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-            // Создаём массив пар: (значение в 4-й строке, индекс столбца)
-            var valuesAndIndices = new (int value, int colIndex)[cols];
-
-            for (int j = 0; j < cols; j++)
+            // Проверяем, что матрица имеет достаточно столбцов
+            if (cols < 4)
             {
-                valuesAndIndices[j] = (matrix[3, j], j);
+                throw new ArgumentException("Матрица должна иметь как минимум 4 столбца");
             }
 
-            // Сортируем столбцы по значению в 4-й строке (индекс 3)
-            Array.Sort(valuesAndIndices, (a, b) => a.value.CompareTo(b.value));
+            // Создаем копию исходной матрицы
+            int[,] resultMatrix = (int[,])matrix.Clone();
 
-            // Создаём результат: переставляем столбцы в новом порядке
-            int[,] result = new int[rows, cols];
-
-            for (int j = 0; j < cols; j++)
+            // Сортируем 4-й столбец (индекс 3) по возрастанию
+            for (int i = 0; i < rows - 1; i++)
             {
-                int sourceColIndex = valuesAndIndices[j].colIndex;
-                for (int i = 0; i < rows; i++)
+                for (int j = 0; j < rows - i - 1; j++)
                 {
-                    result[i, j] = matrix[i, sourceColIndex];
+                    if (resultMatrix[j, 3] > resultMatrix[j + 1, 3])
+                    {
+                        // Меняем местами элементы в 4-м столбце
+                        int temp = resultMatrix[j, 3];
+                        resultMatrix[j, 3] = resultMatrix[j + 1, 3];
+                        resultMatrix[j + 1, 3] = temp;
+
+                        // Также меняем соответствующие строки, чтобы сохранить целостность данных
+                        for (int k = 0; k < cols; k++)
+                        {
+                            if (k != 3) // Не трогаем 4-й столбец, так как мы его уже поменяли
+                            {
+                                int tempRow = resultMatrix[j, k];
+                                resultMatrix[j, k] = resultMatrix[j + 1, k];
+                                resultMatrix[j + 1, k] = tempRow;
+                            }
+                        }
+                    }
                 }
             }
 
-            return result;
+            return resultMatrix;
         }
     }
 }
