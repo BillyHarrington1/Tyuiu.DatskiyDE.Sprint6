92b78907830c2b73d9cdd4fde6f6bd66d142a95d
BillyHarrington1 <datskiydenis@gmail.com>
2025-11-15 09:37:16 +0500

    Task3

--- Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
+++ Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib/DataService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using tyuiu.cources.programming.interfaces.Sprint6;
-
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib
 {
     public class DataService : ISprint6Task3V27
@@ -12,27 +10,21 @@ namespace Tyuiu.DatskiyDE.Sprint6.Task3.V27.Lib
 
             if (cols < 4)
             {
-                throw new ArgumentException("Матрица должна иметь как минимум 4 столбца");
-            }
-
-            // Создаем копию исходной матрицы
-            int[,] resultMatrix = (int[,])matrix.Clone();
-
-            // Сортируем только 4-й столбец (индекс 3), оставляя остальные элементы на своих местах
+                throw new ArgumentException("Матрица должна иметь 5 столбцов");
+            }      
+            int[,] resultMatrix = (int[,])matrix.Clone();       
             for (int i = 0; i < rows - 1; i++)
             {
                 for (int j = 0; j < rows - i - 1; j++)
                 {
                     if (resultMatrix[j, 3] > resultMatrix[j + 1, 3])
                     {
-                        // Меняем местами только элементы в 4-м столбце
                         int temp = resultMatrix[j, 3];
                         resultMatrix[j, 3] = resultMatrix[j + 1, 3];
                         resultMatrix[j + 1, 3] = temp;
                     }
                 }
             }
-
             return resultMatrix;
         }
     }
