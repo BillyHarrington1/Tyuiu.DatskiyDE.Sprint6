ffc7755af75fbb0b2f6d60510236baab4e05762a
BillyHarrington1 <datskiydenis@gmail.com>
2025-11-21 12:44:42 +0500

    Task4

--- Tyuiu.DatskiyDE.Sprint6.Task4.V5/FormMain.Designer.cs
+++ Tyuiu.DatskiyDE.Sprint6.Task4.V5/FormMain.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBoxCondition_DDE = new System.Windows.Forms.GroupBox();
+            this.labelFormula_DDE = new System.Windows.Forms.Label();
+            this.groupBoxInput_DDE = new System.Windows.Forms.GroupBox();
+            this.buttonHelp_DDE = new System.Windows.Forms.Button();
+            this.buttonCalculate_DDE = new System.Windows.Forms.Button();
+            this.buttonSave_DDE = new System.Windows.Forms.Button();
+            this.groupBoxOutput_DDE = new System.Windows.Forms.GroupBox();
+            this.textBoxResult_DDE = new System.Windows.Forms.TextBox();
+            this.cartesianChartResult_DDE = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // groupBoxCondition_DDE
+            // 
+            this.groupBoxCondition_DDE.Controls.Add(this.labelFormula_DDE);
+            this.groupBoxCondition_DDE.Location = new System.Drawing.Point(30, 20);
+            this.groupBoxCondition_DDE.Name = "groupBoxCondition_DDE";
+            this.groupBoxCondition_DDE.Size = new System.Drawing.Size(740, 80);
+            this.groupBoxCondition_DDE.TabIndex = 0;
+            this.groupBoxCondition_DDE.TabStop = false;
+            this.groupBoxCondition_DDE.Text = "Условие";
+            // 
+            // labelFormula_DDE
+            // 
+            this.labelFormula_DDE.AutoSize = true;
+            this.labelFormula_DDE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFormula_DDE.Location = new System.Drawing.Point(30, 30);
+            this.labelFormula_DDE.Name = "labelFormula_DDE";
+            this.labelFormula_DDE.Size = new System.Drawing.Size(300, 20);
+            this.labelFormula_DDE.TabIndex = 0;
+            this.labelFormula_DDE.Text = "F(x) = (2*sin(x))/(3x+1.2) + cos(x) - 14x";
+            // 
+            // groupBoxInput_DDE
+            // 
+            this.groupBoxInput_DDE.Controls.Add(this.buttonHelp_DDE);
+            this.groupBoxInput_DDE.Controls.Add(this.buttonCalculate_DDE);
+            this.groupBoxInput_DDE.Controls.Add(this.buttonSave_DDE);
+            this.groupBoxInput_DDE.Location = new System.Drawing.Point(30, 110);
+            this.groupBoxInput_DDE.Name = "groupBoxInput_DDE";
+            this.groupBoxInput_DDE.Size = new System.Drawing.Size(740, 100);
+            this.groupBoxInput_DDE.TabIndex = 1;
+            this.groupBoxInput_DDE.TabStop = false;
+            this.groupBoxInput_DDE.Text = "Ввод данных";
+            // 
+            // buttonHelp_DDE
+            // 
+            this.buttonHelp_DDE.Location = new System.Drawing.Point(220, 27);
+            this.buttonHelp_DDE.Name = "buttonHelp_DDE";
+            this.buttonHelp_DDE.Size = new System.Drawing.Size(80, 23);
+            this.buttonHelp_DDE.TabIndex = 0;
+            this.buttonHelp_DDE.Text = "Справка";
+            this.buttonHelp_DDE.UseVisualStyleBackColor = true;
+            this.buttonHelp_DDE.Click += new System.EventHandler(this.buttonHelp_DDE_Click);
+            // 
+            // buttonCalculate_DDE
+            // 
+            this.buttonCalculate_DDE.Location = new System.Drawing.Point(320, 27);
+            this.buttonCalculate_DDE.Name = "buttonCalculate_DDE";
+            this.buttonCalculate_DDE.Size = new System.Drawing.Size(80, 23);
+            this.buttonCalculate_DDE.TabIndex = 1;
+            this.buttonCalculate_DDE.Text = "Выполнить";
+            this.buttonCalculate_DDE.UseVisualStyleBackColor = true;
+            this.buttonCalculate_DDE.Click += new System.EventHandler(this.buttonCalculate_DDE_Click);
+            // 
+            // buttonSave_DDE
+            // 
+            this.buttonSave_DDE.Location = new System.Drawing.Point(420, 27);
+            this.buttonSave_DDE.Name = "buttonSave_DDE";
+            this.buttonSave_DDE.Size = new System.Drawing.Size(80, 23);
+            this.buttonSave_DDE.TabIndex = 2;
+            this.buttonSave_DDE.Text = "Сохранить";
+            this.buttonSave_DDE.UseVisualStyleBackColor = true;
+            this.buttonSave_DDE.Click += new System.EventHandler(this.buttonSave_DDE_Click);
+            // 
+            // groupBoxOutput_DDE
+            // 
+            this.groupBoxOutput_DDE.Controls.Add(this.textBoxResult_DDE);
+            this.groupBoxOutput_DDE.Controls.Add(this.cartesianChartResult_DDE);
+            this.groupBoxOutput_DDE.Location = new System.Drawing.Point(30, 220);
+            this.groupBoxOutput_DDE.Name = "groupBoxOutput_DDE";
+            this.groupBoxOutput_DDE.Size = new System.Drawing.Size(740, 200);
+            this.groupBoxOutput_DDE.TabIndex = 2;
+            this.groupBoxOutput_DDE.TabStop = false;
+            this.groupBoxOutput_DDE.Text = "Вывод данных";
+            // 
+            // textBoxResult_DDE
+            // 
+            this.textBoxResult_DDE.Location = new System.Drawing.Point(30, 30);
+            this.textBoxResult_DDE.Multiline = true;
+            this.textBoxResult_DDE.Name = "textBoxResult_DDE";
+            this.textBoxResult_DDE.ReadOnly = true;
+            this.textBoxResult_DDE.Size = new System.Drawing.Size(300, 150);
+            this.textBoxResult_DDE.TabIndex = 0;
+            // 
+            // cartesianChartResult_DDE
+            // 
+            this.cartesianChartResult_DDE.Location = new System.Drawing.Point(350, 30);
+            this.cartesianChartResult_DDE.Name = "cartesianChartResult_DDE";
+            this.cartesianChartResult_DDE.Size = new System.Drawing.Size(350, 150);
+            this.cartesianChartResult_DDE.TabIndex = 1;
+            this.cartesianChartResult_DDE.Text = "cartesianChart1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBoxOutput_DDE);
+            this.Controls.Add(this.groupBoxInput_DDE);
+            this.Controls.Add(this.groupBoxCondition_DDE);
+            this.Name = "FormMain";
+            this.Text = "Спринт 6 | Task 4 | Вариант 5 | Дацкий Д. Е.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxCondition_DDE;
+        private System.Windows.Forms.Label labelFormula_DDE;
+        private System.Windows.Forms.GroupBox groupBoxInput_DDE;
+        private System.Windows.Forms.Button buttonHelp_DDE;
+        private System.Windows.Forms.Button buttonCalculate_DDE;
+        private System.Windows.Forms.Button buttonSave_DDE;
+        private System.Windows.Forms.GroupBox groupBoxOutput_DDE;
+        private System.Windows.Forms.TextBox textBoxResult_DDE;
+        private LiveCharts.WinForms.CartesianChart cartesianChartResult_DDE;
     }
 }
--- Tyuiu.DatskiyDE.Sprint6.Task4.V5/FormMain.cs
+++ Tyuiu.DatskiyDE.Sprint6.Task4.V5/FormMain.cs
@@ -1,3 +1,11 @@
+using LiveCharts;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Tyuiu.DatskiyDE.Sprint6.Task4.V5.Lib;
+
 namespace Tyuiu.DatskiyDE.Sprint6.Task4.V5
 {
     public partial class FormMain : Form
@@ -5,6 +13,96 @@ namespace Tyuiu.DatskiyDE.Sprint6.Task4.V5
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void buttonCalculate_DDE_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+
+            try
+            {
+                int startValue = -5;
+                int stopValue = 5;
+
+                double[] values = ds.GetMassFunction(startValue, stopValue);
+
+                string table = "+-----+--------+\n";
+                table += "|  X  |  f(x)  |\n";
+                table += "+-----+--------+\n";
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    int x = startValue + i;
+                    table += $"| {x,3} | {values[i],6:F2} |\n";
+                }
+
+                table += "+-----+--------+";
+
+                textBoxResult_DDE.Text = table;
+
+                // Построение графика с LiveCharts
+                var series = new LineSeries
+                {
+                    Values = new ChartValues<double>(),
+                    PointGeometrySize = 0,
+                    StrokeThickness = 2,             
+                };
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    int x = startValue + i;
+                    series.Values.Add(values[i]);
+                }
+
+                cartesianChartResult_DDE.Series.Clear();
+                cartesianChartResult_DDE.Series.Add(series);
+
+                // Настройка осей
+                cartesianChartResult_DDE.AxisX.Clear();
+                cartesianChartResult_DDE.AxisY.Clear();
+
+                cartesianChartResult_DDE.AxisX.Add(new Axis { Title = "X", MinValue = -6, MaxValue = 6 });
+                cartesianChartResult_DDE.AxisY.Add(new Axis { Title = "f(x)", MinValue = -100, MaxValue = 100 });
+
+                // Обновляем график
+                cartesianChartResult_DDE.Update();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при вычислении!");
+            }
+        }
+
+        private void buttonSave_DDE_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double[] values = ds.GetMassFunction(startValue, stopValue);
+
+            // Сохраняем в файл
+            string tempPath = Path.GetTempPath();
+            string fileName = "OutPutFileTask4V5.txt";
+            string filePath = Path.Combine(tempPath, fileName);
+
+            string content = "";
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                int x = startValue + i;
+                content += $"{x};{values[i]:F2}\n";
+            }
+
+            File.WriteAllText(filePath, content);
+
+            MessageBox.Show($"Файл сохранён по пути: {filePath}");
+        }
+
+        private void buttonHelp_DDE_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Табулировать функцию F(x) = (2*sin(x))/(3x+1.2) + cos(x) - 14x\nна диапазоне [-5, 5] с шагом 1.");
         }
     }
 }
